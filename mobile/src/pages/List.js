7677a604588d129f3fbb79483926494e9ecd4a19
Matheus Machado <matheus_lm10@hotmail.com>
2020-06-21 00:07:41 -0400

    Resposta do dono do spot para a solicitação de reserva.

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Image, Text, AsyncStorage, TouchableOpacity } from 'react-native';
+import socketio from 'socket.io-client';
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Image, Text, AsyncStorage, TouchableOpacity } from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -7,6 +8,20 @@ import logo from '../assets/logo.png';
 
 export default function List({ navigation }) {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.0.23/3333', {
+                query: { user_id }
+            })
+
+            socket.on('booking_response', booking => {
+                console.log('booking_response', booking)
+                Alert.alert(`Sua reserva em ${booking.spot.company} para o dia ${booking.date} 
+                foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+            })
+        })
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storagedTechs => {
@@ -35,11 +50,10 @@ export default function List({ navigation }) {
                     ? (techs.map(tech => <SpotList key={tech} tech={tech} />))
                     : (<Text>Não existem tecnologias.</Text>)
                 }
+                <TouchableOpacity onPress={handleLogOut} style={styles.button}>
+                    <Text style={styles.buttonText}>LogOut</Text>
+                </TouchableOpacity>
             </ScrollView>
-            
-            {/* <TouchableOpacity onPress={handleLogOut} style={styles.button}>
-                <Text style={styles.buttonText}>LogOut</Text>
-            </TouchableOpacity> */}
 
         </SafeAreaView>
     )
@@ -62,7 +76,8 @@ const styles = StyleSheet.create({
         backgroundColor: "#f05a5b",
         justifyContent: "center",
         alignItems: "center",
-        borderRadius: 2
+        borderRadius: 2,
+        marginTop: 20
     },
 
     buttonText: {
